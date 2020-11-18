9e2d016b4e49667a907dcb2576a821dc106c3a66
bobur-Kobilov <bobur0114jon@gmail.com>
2020-11-18 09:57:22 +0900

    mysql script added

--- persistence/db.sql
+++ persistence/db.sql
@@ -1,12 +1,26 @@
-CREATE TABLE user (
-  email varchar(128) UNIQUE, 
+CREATE TABLE user
+(
+  email varchar(128) UNIQUE,
   password varchar(128),
   first_name varchar(256),
-  last_name varchar(256), 
+  last_name varchar(256),
   org_name varchar(256),
   inst varchar(256) default '',
   build_no varchar(128) default '',
   floor_no varchar(256) default '',
   lab_head varchar(128) default '',
   lab_address varchar(128) default '',
-  tel varchar(128) default '');
+  tel varchar(128) default ''
+);
+
+
+
+CREATE TABLE data
+(
+  name varchar(128) NOT NULL,
+  desciption varchar(256) NOT NULL,
+  code varchar(20) NOT NULL,
+  producedAt timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  createdAt timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY code (code)
+);
